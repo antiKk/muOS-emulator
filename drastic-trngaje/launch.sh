d56b3900865a3f9f6cbfbd7cfcb84c9b8672f91f
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-05-09 21:47:55 +1000

    Updated drastic-trngaje for TrimUI devices

--- drastic-trngaje/launch.sh
+++ drastic-trngaje/launch.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+. /opt/muos/script/var/func.sh
+
 DRASTIC_DIR=$(dirname "$0")
-export LD_LIBRARY_PATH=$DRASTIC_DIR/libs:$LD_LIBRARY_PATH
+DRASTIC_LIB="$DRASTIC_DIR/libs"
+
+case "$(GET_VAR "device" "board/name")" in
+	rg*) DRASTIC_LIB="${DRASTIC_LIB}/rg" ;;
+	tui*) DRASTIC_LIB="${DRASTIC_LIB}/tui" ;;
+esac
+
+export LD_LIBRARY_PATH=$DRASTIC_LIB:$LD_LIBRARY_PATH
 
 cd "$DRASTIC_DIR" || exit 1
 ./drastic "$1"
